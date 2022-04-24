ecfebfed0d4362d18f463ab4102bb51731b6ce3a
disservin <45608332+Disservin@users.noreply.github.com>
2022-04-24 10:09:18 +0200

    Update search.cpp
    Bench: 7591330

--- src/search.cpp
+++ src/search.cpp
@@ -1169,7 +1169,7 @@ moves_loop: // When in check, search starts here
           
           // Increase reduction if the previous move was a null move.
           if ((ss-1)->currentMove == MOVE_NULL && depth >= 4)
-              r+=2;
+              r++;
 
           // Decrease reduction at PvNodes if bestvalue
           // is vastly different from static evaluation
