e63f90863a2a3fa813be63df828eceaf43a76c4e
disservin <45608332+Disservin@users.noreply.github.com>
2022-05-03 18:22:57 +0200

    Update search.cpp
    Bench: 7905883

--- src/search.cpp
+++ src/search.cpp
@@ -1144,6 +1144,7 @@ moves_loop: // When in check, search starts here
       if (    depth >= 2
           &&  moveCount > 1 + (PvNode && ss->ply <= 1)
           && (   !ss->ttPv
+              || !PvNode
               || !capture
               || (cutNode && (ss-1)->moveCount > 1)))
       {
