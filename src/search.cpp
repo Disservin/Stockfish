c450ec7a182d6f0daaa2370f688eaab04029b6c9
disservin <45608332+Disservin@users.noreply.github.com>
2022-06-03 23:24:28 +0200

    Update search.cpp
    Bench: 6237567

--- src/search.cpp
+++ src/search.cpp
@@ -465,7 +465,7 @@ void Thread::search() {
           && !mainThread->stopOnPonderhit)
       {
           int effort = (mainThread->spentEffort[from_sq(rootMoves[0].pv[0])][to_sq(rootMoves[0].pv[0])] * 100) / (mainThread->nodes);
-          float effortScaling = (120 - std::min(effort, 40)) / 100.0f;
+          float effortScaling = (110 - std::min(effort, 90)) / 100.0f;
 
           double fallingEval = (69 + 12 * (mainThread->bestPreviousAverageScore - bestValue)
                                     +  6 * (mainThread->iterValue[iterIdx] - bestValue)) / 781.4;
