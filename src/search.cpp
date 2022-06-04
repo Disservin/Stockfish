4aa7387834f6a7f8c7f553008e01451686b6f85a
disservin <45608332+Disservin@users.noreply.github.com>
2022-06-04 08:50:28 +0200

    effort 50 base
    Bench: 6237567

--- src/search.cpp
+++ src/search.cpp
@@ -465,7 +465,7 @@ void Thread::search() {
           && !mainThread->stopOnPonderhit)
       {
           int effort = (mainThread->spentEffort[from_sq(rootMoves[0].pv[0])][to_sq(rootMoves[0].pv[0])] * 100) / (mainThread->nodes);
-          int adjustHighEffort = effort > 80 ? 50 : 40;
+          int adjustHighEffort = effort >= 95 ? 10 : 50;
           double effortScaling = std::pow(0.5, 0.01*(effort - adjustHighEffort));
 
           double fallingEval = (69 + 12 * (mainThread->bestPreviousAverageScore - bestValue)
