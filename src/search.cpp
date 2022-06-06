c891a90fe5c9fe5b3290ef981cf663c998fab954
disservin <45608332+Disservin@users.noreply.github.com>
2022-06-06 22:25:22 +0200

    Update search.cpp
    Bench: 6237567

--- src/search.cpp
+++ src/search.cpp
@@ -792,7 +792,7 @@ namespace {
     // Step 8. Futility pruning: child node (~25 Elo).
     // The depth condition is important for mate finding.
     if (   !ss->ttPv
-        &&  depth < 8
+        &&  depth < 8 + (thisThread->id() % 2 == 1)
         &&  eval - futility_margin(depth, improving) - (ss-1)->statScore / 256 >= beta
         &&  eval >= beta
         &&  eval < 26305) // larger than VALUE_KNOWN_WIN, but smaller than TB wins.
