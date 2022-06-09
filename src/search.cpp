8c82e9f237b866401b0db0e1243397f73db08e2a
disservin <45608332+Disservin@users.noreply.github.com>
2022-06-09 08:29:25 +0200

    Update search.cpp
    Bench: 5792031

--- src/search.cpp
+++ src/search.cpp
@@ -1350,7 +1350,7 @@ moves_loop: // When in check, search starts here
 
     assert(moveCount || !ss->inCheck || excludedMove || !MoveList<LEGAL>(pos).size());
     
-    (ss+1)->disableNullMove = 10 * nullmoveCount > 7 * moveCount && rootNode;
+    (ss+1)->disableNullMove = 10 * nullmoveCount > 8 * moveCount && rootNode;
 
     if (!moveCount)
         bestValue = excludedMove ? alpha :
