429e51ab113451e160ee32833e9fdfbf449fe17e
disservin <45608332+Disservin@users.noreply.github.com>
2022-06-20 12:46:35 +0200

    Update search.cpp
    Bench: 4832967

--- src/search.cpp
+++ src/search.cpp
@@ -1156,6 +1156,9 @@ moves_loop: // When in check, search starts here
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode && move != ss->killers[0])
               r += 2;
+          
+          if((ss-1)->moveCount < 3 && moveCount > 4)
+              r++;
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
