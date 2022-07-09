cda9312a581cbb98e50b74ac6ff3768aa54c135f
disservin <45608332+Disservin@users.noreply.github.com>
2022-07-09 21:23:20 +0200

    Update search.cpp
    Bench: 5261783

--- src/search.cpp
+++ src/search.cpp
@@ -1170,6 +1170,12 @@ moves_loop: // When in check, search starts here
           // Increase reduction if next ply has a lot of fail high else reset count to 0
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
+          
+          // Decrease reduction for checks while in check
+          if (   ss->inCheck
+              && givesCheck
+              && depth < 4)
+              r--;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
