f969facee95d2cc5551cc29097b089b8c8c194b9
disservin <45608332+Disservin@users.noreply.github.com>
2022-07-19 22:48:06 +0200

    Update search.cpp
    Bench: 5942086

--- src/search.cpp
+++ src/search.cpp
@@ -1342,7 +1342,7 @@ moves_loop: // When in check, search starts here
       }
     }
 
-    if (ss->nullmoveCnt >= moveCount - 2 && moveCount - 2 > 0)
+    if (ss->nullmoveCnt >= moveCount - 4 && moveCount - 4 > 0 && ss->ply < 6)
         thisThread->nullmoveAllowed = false;
 
     // The following condition would detect a stop only after move loop has been
