6ea77124c58888c23916ee35e718277176e903c9
disservin <45608332+Disservin@users.noreply.github.com>
2022-07-26 17:06:44 +0200

    Update evaluate.cpp
    Bench: 5900335

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1055,15 +1055,15 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
   Value psq = pos.psq_eg_stm();
   // Deciding between classical and NNUE eval (~10 Elo): for high PSQ imbalance we use classical,
   // but we switch to NNUE during long shuffling or with high material on the board.
-  bool useClassical =    (pos.this_thread()->depth > 9 || pos.count<ALL_PIECES>() > 8)
-                      && abs(psq) * 6 > (1045 + pos.non_pawn_material() / 64) * (10 + pos.rule50_count());
+  bool useClassical =    (pos.this_thread()->depth > 10 || pos.count<ALL_PIECES>() > 9)
+                      && abs(psq) * 6 > (1007 + pos.non_pawn_material() / 76) * (10 + pos.rule50_count());
 
   // Deciding between classical and NNUE eval (~10 Elo): for high PSQ imbalance we use classical,
   // but we switch to NNUE during long shuffling or with high material on the board.
   if (!useNNUE || useClassical)
   {
       v = Evaluation<NO_TRACE>(pos).value();
-      useClassical = abs(v) >= 313;
+      useClassical = abs(v) >= 287;
   }
 
   // If result of a classical evaluation is much lower than threshold fall back to NNUE
