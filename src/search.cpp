9d1e080bbc58a89e5477691cd70ce6364e75f484
disservin <45608332+Disservin@users.noreply.github.com>
2022-09-18 22:02:38 +0200

    Update search.cpp
    Bench: 4379669

--- src/search.cpp
+++ src/search.cpp
@@ -1164,7 +1164,7 @@ moves_loop: // When in check, search starts here
               r--;
 
           if (!capture && mp.positionalThreat & from_sq(move) && quietCount > 3 * depth)
-              r++;
+              r--;
 
           // Increase reduction if next ply has a lot of fail high else reset count to 0
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
