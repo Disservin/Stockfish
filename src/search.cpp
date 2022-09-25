d0f54e5efc571b88df7c92f7a1143b2a047571e9
disservin <45608332+Disservin@users.noreply.github.com>
2022-09-25 11:51:20 +0200

    Update search.cpp
    Bench: 4355101

--- src/search.cpp
+++ src/search.cpp
@@ -780,7 +780,7 @@ namespace {
     // If eval is really low check with qsearch if it can exceed alpha, if it can't,
     // return a fail low.
     if (   depth <= 8
-        && eval < alpha - 369 - 254 * depth * depth)
+        && eval < alpha - constval - constmult * depth * depth)
     {
         value = qsearch<NonPV>(pos, ss, alpha - 1, alpha);
         if (value < alpha)
