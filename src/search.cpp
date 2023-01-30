1eb772a12e405b8acc46eba6717f1c776d8cf741
disservin <disservin.social@gmail.com>
2023-01-30 20:08:24 +0100

    Update search.cpp
    Bench: 3806682

--- src/search.cpp
+++ src/search.cpp
@@ -1163,6 +1163,9 @@ moves_loop: // When in check, search starts here
       // Increase reduction if next ply has a lot of fail high
       if ((ss+1)->cutoffCnt > 3)
           r++;
+      
+      if (ss->ply < 4 && type_of(movedPiece) == KING)
+          r++;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
