ca8397fa02013022a4872e22cf48ddac28e84ed4
Disservin <disservin.social@gmail.com>
2023-08-26 16:07:43 +0200

    Bench: 1444293

--- src/search.cpp
+++ src/search.cpp
@@ -1152,11 +1152,14 @@ moves_loop: // When in check, search starts here
           && pos.has_repeated())
           r += 2;
 
-      if (ss->key == (ss-4)->key && moveCount == 1)
+      if (ss->key == (ss-4)->key)
       {
-          r += 2;
-          depth++;
+          if (moveCount == 1)
+              r++;
+          else
+              r -= moveCount < 5;
       }
+
 
       // Increase reduction if next ply has a lot of fail high (~5 Elo)
       if ((ss+1)->cutoffCnt > 3)
