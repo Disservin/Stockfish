c637728eb05352f5d1c6ebc7591cbbd4e68ee7f1
Disservin <disservin.social@gmail.com>
2023-12-31 16:01:59 +0100

    Update types.h

--- src/types.h
+++ src/types.h
@@ -161,7 +161,7 @@ enum Bound : uint8_t {
     BOUND_EXACT = BOUND_UPPER | BOUND_LOWER
 };
 
-enum Value : int16_t {
+enum Value : int {
     VALUE_ZERO     = 0,
     VALUE_DRAW     = 0,
     VALUE_NONE     = 32002,
