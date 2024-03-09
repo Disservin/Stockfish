254fdc4a0ea7ceaf366519b456d407f2e66ca3a3
Disservin <disservin.social@gmail.com>
2024-03-09 23:05:02 +0100

    remove duplicated evalfile macro

--- src/nnue/network.cpp
+++ src/nnue/network.cpp
@@ -26,9 +26,6 @@ namespace {
 //     const unsigned int         gEmbeddedNNUESize;    // the size of the embedded file
 // Note that this does not work in Microsoft Visual Studio.
 #if !defined(_MSC_VER) && !defined(NNUE_EMBEDDING_OFF)
-    #define EvalFileDefaultNameBig "nn-1ceb1ade0001.nnue"
-    #define EvalFileDefaultNameSmall "nn-baff1ede1f90.nnue"
-
 INCBIN(EmbeddedNNUEBig, EvalFileDefaultNameBig);
 INCBIN(EmbeddedNNUESmall, EvalFileDefaultNameSmall);
 #else
