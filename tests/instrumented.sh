15880264fb61684dff1c1b44d6f4102e1d4e380a
Disservin <disservin.social@gmail.com>
2024-03-14 10:26:46 +0100

    wip

--- tests/instrumented.sh
+++ tests/instrumented.sh
@@ -101,7 +101,7 @@ ls -la
 
 uname -r
 
-gdb -batch -ex "run" -ex "bt" --args ./stockfish bench 2>&1 | grep -v ^"No stack."$
+gdb -batch -ex "run" -ex "bt" --args ./stockfish uci 2>&1 | grep -v ^"No stack."$
 
 ./stockfish uci
 
