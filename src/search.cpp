5e74b1e2594cd327436eee3c10c5539a4a9638dc
Disservin <disservin.social@gmail.com>
2024-12-24 20:13:26 +0100

    Test 1.9 Bench: 1294909

--- src/search.cpp
+++ src/search.cpp
@@ -467,7 +467,7 @@ void Search::Worker::iterative_deepening() {
                 threads.stop = true;
 
             // calculate branching factor on the fly?
-            std::size_t nodesNextIteration = std::exp((rootDepth + 1) * std::log(1.8));
+            std::size_t nodesNextIteration = std::exp((rootDepth + 1) * std::log(1.9));
 
             if (nodesNextIteration > nodes)
             {
